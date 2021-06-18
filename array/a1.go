3fd88c2a81cdf20a230cbf6a76bbef401f2269c6
gaurav namdev <gauravkumarayoconnect.id>
2021-06-18 16:24:49 +0530

    aa

--- array/a1.go
+++ array/a1.go
@@ -8,6 +8,6 @@ func main() {
 	var a [3]bool //int array with length 3
 	fmt.Println(a)
 
-	b := [...]int8{1, 2, 3, 4} //int array with length 3
+	b := [...]int8{1, 2, 3, 4, 5, 6} //int array with length 3
 	fmt.Println(b)
 }
